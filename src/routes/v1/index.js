c3f78f089b3b7980f93938dc717a44d5630971c2
LoiTang <heobovidai@gmail.com>
2025-11-25 19:21:29 +0700

    route-v1

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,5 +1,13 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import express from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { boardRoutes } from './boardRoutes'
+
+const Router = express.Router()
+
+// 2 tham so duoc truyen vao: 1. api-endpoint, 2-callback function
+Router.get('/status', (req, res) => {
+  res.status(StatusCodes.OK).json({ message: 'Apis v1 ready...' })
+})
+Router.use('/boards', boardRoutes)
+
+export const APIs_V1 = Router
